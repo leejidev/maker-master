0f03d57baa7697cd2bc78178363d6292bb1df6f2
leeji <leejidev@gmail.com>
2025-11-08 02:37:36 +0900

    feat(community): load posts/topics via drizzle queries and enable db logging

--- app/db.ts
+++ app/db.ts
@@ -3,6 +3,6 @@ import postgres from "postgres";
 
 const client = postgres(process.env.DATABASE_URL!, { prepare: false }); // Connection Pooling 기능을 Supabase는 지원하고 Drizzle는 지원하지 않기 때문에 prepare: false 설정
 
-const db = drizzle(client);
+const db = drizzle(client, { logger: true });
 
 export default db;
--- app/features/community/components/post-card.tsx
+++ app/features/community/components/post-card.tsx
@@ -13,14 +13,15 @@ import {
 } from "~/common/components/ui/avatar";
 import { ChevronUpIcon, DotIcon } from "lucide-react";
 import { cn } from "~/lib/utils";
+import { DateTime } from "luxon";
 
 interface PostCardProps {
-  id: string;
+  id: number;
   title: string;
   author: string;
   category: string;
-  postedAt: string;
-  authorAvatarUrl?: string;
+  postedAt: Date;
+  authorAvatarUrl: string | null;
   expanded?: boolean;
   votesCount?: number;
 }
@@ -54,7 +55,7 @@ export function PostCard({
               <span>{author} on</span>
               <span>{category}</span>
               <span>•</span>
-              <span>{postedAt}</span>
+              <span>{DateTime.fromJSDate(postedAt).toRelative()}</span>
             </div>
           </div>
         </CardHeader>
