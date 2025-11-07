2927a4ca76686842e17665ae268de7da8c0d9ba2
leeji <leejidev@gmail.com>
2025-11-07 23:23:37 +0900

    feat(db): add products schema with categories, upvotes and reviews
    - Define products, categories, product_upvotes, and reviews tables with constraints
    - Add migration for products schema and relationships
    - Update migration snapshot and journal metadata

---
+++ app/features/products/schema.ts
@@ -0,0 +1,86 @@
+import {
+  bigint,
+  check,
+  integer,
+  jsonb,
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
+import { profiles } from "../users/schema";
+import { sql } from "drizzle-orm";
+
+export const products = pgTable("products", {
+  product_id: bigint({ mode: "number" })
+    .primaryKey()
+    .generatedAlwaysAsIdentity(),
+  name: text().notNull(),
+  tagline: text().notNull(),
+  description: text().notNull(),
+  how_it_works: text().notNull(),
+  icon: text().notNull(),
+  url: text().notNull(),
+  stats: jsonb().notNull().default({ views: 0, reviews: 0 }),
+  profile_id: uuid()
+    .references(() => profiles.profile_id, { onDelete: "cascade" })
+    .notNull(),
+  category_id: bigint({ mode: "number" }).references(
+    () => categories.category_id,
+    { onDelete: "set null" }
+  ),
+  created_at: timestamp().notNull().defaultNow(),
+  updated_at: timestamp().notNull().defaultNow(),
+});
+
+export const categories = pgTable("categories", {
+  category_id: bigint({ mode: "number" })
+    .primaryKey()
+    .generatedAlwaysAsIdentity(),
+  name: text().notNull(),
+  description: text().notNull(),
+  created_at: timestamp().notNull().defaultNow(),
+  updated_at: timestamp().notNull().defaultNow(),
+});
+
+export const product_upvotes = pgTable(
+  "product_upvotes",
+  {
+    product_id: bigint({ mode: "number" }).references(
+      () => products.product_id,
+      {
+        onDelete: "cascade",
+      }
+    ),
+    profile_id: uuid().references(() => profiles.profile_id, {
+      onDelete: "cascade",
+    }),
+  },
+  // 제약 조건 설정: 두 개의 컬럼을 기반으로 기본 키 설정
+  table => [primaryKey({ columns: [table.product_id, table.profile_id] })]
+);
+
+export const reviews = pgTable(
+  "reviews",
+  {
+    review_id: bigint({ mode: "number" })
+      .primaryKey()
+      .generatedAlwaysAsIdentity(),
+    product_id: bigint({ mode: "number" }).references(
+      () => products.product_id,
+      {
+        onDelete: "cascade",
+      }
+    ),
+    profile_id: uuid().references(() => profiles.profile_id, {
+      onDelete: "cascade",
+    }),
+    rating: integer().notNull(),
+    review: text().notNull(),
+    created_at: timestamp().notNull().defaultNow(),
+    updated_at: timestamp().notNull().defaultNow(),
+  },
+  // 제약 조건 설정: rating 컬럼의 값이 1과 5 사이인지 확인 쿼리 설정
+  table => [check("rating_check", sql`${table.rating} BETWEEN 1 AND 5`)]
+);
